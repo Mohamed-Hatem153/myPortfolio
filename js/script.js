ed47179e3b32f77d276cde51660cf807e75b58b1
Mohamed Hatem <mohamedhatemakl@gmail.com>
2021-06-30 01:17:38 +0200

    complete navbar, toggler btn and links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+/*--------- Toogle navbar ---------*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavebar();
+  document.body.classList.toggle(".hide-scrolling");
+});
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavebar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+/*-------- Active section --------*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavebar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+
 /*--------- About Tabs------*/
 const tabsContainer = document.querySelector(".about-tabs"),
   aboutSection = document.querySelector(".about-section");
